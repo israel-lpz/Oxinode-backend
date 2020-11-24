fb84154e19a932a4100c8ddf9e15c301d550192f
Israel Lopez <demonsdark298@gmail.com>
2020-11-24 09:47:26 -0600

    Delete build folder

--- build/Config/configCloudinary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const cloudinary_1 = tslib_1.__importDefault(require("cloudinary"));
-cloudinary_1.default.v2.config({
-    cloud_name: process.env.cloudinary_db_name,
-    api_key: process.env.cloudinary_api_key,
-    api_secret: process.env.cloudinary_api_secret,
-});
